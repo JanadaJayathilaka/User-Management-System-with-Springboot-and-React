bb642ab59971bd314fa28b480b2cb72a733b8993
janada jayathilaka <86776703+JanadaJayathilaka@users.noreply.github.com>
2024-12-11 17:28:51 +0530

    backend folder

--- backend/src/main/java/com/example/Full/Stack/Backend/controller/UserController.java
+++ backend/src/main/java/com/example/Full/Stack/Backend/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.example.Full.Stack.Backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Full.Stack.Backend.model.User;
 import com.example.Full.Stack.Backend.repository.UserRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,6 +21,11 @@ public class UserController {
     @PostMapping("/user")
     public User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 
 }
